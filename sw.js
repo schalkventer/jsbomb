d90c2e57f39028d9fc5e66c5d8e5a52a09f06c31
Schalk Venter <venterschalk@gmail.com>
2023-03-03 17:31:19 +0200

    Additional SW changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const ID = "74aasddddsd21316cb0";
+const ID = "aa";
 
 const FILES = [
     "/",
@@ -32,6 +32,26 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    console.log(event)
-    event.respondWith(caches.match(event.request));
-});
+    console.log(`Handling fetch event for ${event.request.url}`);
+  
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if (response) {
+          console.log("Found response in cache:", response);
+          return response;
+        }
+        console.log("No response found in cache. About to fetch from network…");
+  
+        return fetch(event.request)
+          .then((response) => {
+            console.log("Response from network is:", response);
+  
+            return response;
+          })
+          .catch((error) => {
+            console.error(`Fetching failed: ${error}`);
+            throw error;
+          });
+      })
+    );
+  });
